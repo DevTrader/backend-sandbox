c3de04847efd8cead805cef25b90e1ddfcc4e6c0
devtrader <nkao552@gmail.com>
2018-07-26 23:46:41 -0700

    improved xray

--- xRayScraper.js
+++ xRayScraper.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 
 const xray = new Xray();
 const url = 'https://www.amazon.com/dp/B074SHSN7P/ref=sspa_dk_detail_1?th=1'
-xray(url, 'li#SalesRank', [{
-    salesRank: ''
-}]).write('result.json');
+// xray(url, 'li#SalesRank', [{
+//     salesRank: ''
+// }]).write('result.json');
+
+xray(url, 'li#SalesRank')((err, data)=>{
+    console.log(data.replace(/(\r\n\t|\n|\r\t)/gm,"")) //regex removes line breaks
+});
